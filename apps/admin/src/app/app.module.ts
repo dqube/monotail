d0be9d85697aaccebca783739689c2b68993ffb0
Dev <mdevendran@outlook.com>
2023-06-02 09:33:00 -0300

    unused modules removed

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,23 +2,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AccordionModule } from '@app/ui/accordion';
-import { AlertModule } from '@app/ui/alert';
-import { ButtonModule } from '@app/ui/button';
-import { CheckboxModule } from '@app/ui/checkbox';
-import { DotsModule } from '@app/ui/dots';
-import { DrawerModule } from '@app/ui/drawer';
-import { DropdownModule } from '@app/ui/dropdown';
-import { InputModule } from '@app/ui/input';
-import { LinkModule } from '@app/ui/link';
-import { ModalModule } from '@app/ui/modal';
-import { RadioModule } from '@app/ui/radio';
-import { SelectModule } from '@app/ui/select';
-import { SidebarModule } from '@app/ui/sidebar';
-import { SpinnersModule } from '@app/ui/spinners';
-import { TableModule } from '@app/ui/table';
-import { TextareaModule } from '@app/ui/textarea';
-import { UploadModule } from '@app/ui/upload';
 import { FormlyModule } from '@form/core';
 import { FormlyTailwindModule } from '@form/tail';
 import { AppComponent } from './app.component';
@@ -30,23 +13,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    AlertModule,
-    ButtonModule,
-    AccordionModule,
-    DropdownModule,
-    DrawerModule,
-    ModalModule,
-    InputModule,
-    TextareaModule,
-    DotsModule,
-    SelectModule,
-    SpinnersModule,
-    UploadModule,
-    LinkModule,
-    CheckboxModule,
-    RadioModule,
-    TableModule,
-    SidebarModule,
+
     ReactiveFormsModule,
     FormlyModule.forRoot({
       validationMessages: [
